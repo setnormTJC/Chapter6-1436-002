111206064e321c7091e3365c92c4e605b176a9b1
Work <Work@MSI>
2023-11-07 15:40:20 -0600

    Where we ended on Nov 7 in the afternoon.

--- Chapter6-1436-002/Source.cpp
+++ Chapter6-1436-002/Source.cpp
@@ -2,54 +2,76 @@
 
 #include<cctype>
 #include "myLibrary.h"
+//#include "myLibrary.h"
 
 using namespace std;
 
 
-
+/*string firstString;*/ 
 
 int main(/*int argc, int[] argv*/)
 {
-    cout << "How many times do you want hello world to get printed?" << endl; 
-    int numberOfTimesToPrint; 
-    cin >> numberOfTimesToPrint; 
+
+    string firstString = "name"; 
+
+    string secondString = "Seth";
+
+    cout << std::boolalpha << isSameStringLength(firstString, secondString) << endl; 
+    //cout << "Enter three numbers - this program determines the largest of those: " << endl; 
+    //double a, b, c; 
+    //cin >> a >> b >> c; 
+
+    //double result = returnLargest(a, b, c);
+
+    //cout << "The largest of the three is: " << result << endl;
+    ////main(); //infinite recursive function call (main calls itself)  
+    //cout << "How many times do you want hello world to get printed?" << endl; 
+    //int numberOfTimesToPrint; 
+    //cin >> numberOfTimesToPrint; 
 
 
-    //call the function `printHelloWorld`
-    printHelloWorld(numberOfTimesToPrint); 
-        
+    ////call the function `printHelloWorld`
+    //printHelloWorld(numberOfTimesToPrint); 
 
-    //cout << ceil(3.4) << "\t" << ceil(-2.4) << endl;
-    //cout << floor(3.4) << "\t" << floor(-2.4) << endl; 
+    ////execution!
 
-    //niche
+    ////cout << ceil(3.4) << "\t" << ceil(-2.4) << endl;
+    ////cout << floor(3.4) << "\t" << floor(-2.4) << endl; 
 
-    //cout << cos(90) << endl; //0?
+    ////niche
 
-    //cout << cos(3.14 / 2) << endl; //0? 
+    ////cout << cos(90) << endl; //0?
 
-    cout << "Enter some character (a digit 0-9), a letter (a - z, A - Z)"
-        << "or something else " << endl;
+    ////cout << cos(3.14 / 2) << endl; //0? 
 
-    char someChar; 
+    //cout << "Enter some character (a digit 0-9), a letter (a - z, A - Z)"
+    //    << "or something else " << endl;
 
-    cin >> someChar; 
+    //char someChar; 
 
-    if (isalpha(someChar))
-    {
-        cout << "You entered a LETTER! Hoorah!" << endl; 
-        cout << "Your letter in UPPERCASE is: " << (char)toupper(someChar) << endl; 
-    }
+    //cin >> someChar; 
 
-    else if (isdigit(someChar))
-    {
-        cout << "You entered a number" << endl; 
-    }
+    //if (isalpha(someChar))
+    //{
+    //    cout << "You entered a LETTER! Hoorah!" << endl; 
+    //    cout << "Your letter in UPPERCASE is: " << (char)toupper(someChar) << endl; 
+    //}
 
-    else
-    {
-        cout << "You entered something else (special character)" << endl; 
-    }
+    //else if (isdigit(someChar))
+    //{
+    //    cout << "You entered a number" << endl; 
+    //}
+
+    //else
+    //{
+    //    cout << "You entered something else (special character)" << endl; 
+    //}
+
+    //cout << largest << endl; 
+    //std::max_element()
+
+
+
 
     return 0;
 }
--- Chapter6-1436-002/myLibrary.h
+++ Chapter6-1436-002/myLibrary.h
@@ -15,3 +15,39 @@ void printHelloWorld(int numberOfTimesToPrint) //
         cout << "Hello, world" << endl;
     }
 }
+
+
+double returnLarger(double num1, double num2)
+//returns the larger of 2 numbers
+{
+    if (num1 >= num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+
+double returnLargest(double num1, double num2, double num3)
+{
+    double largest = 10; //this variable has "local" scope 
+                        //it is only accessible within THIS function (main cannot use it) 
+
+    largest = returnLarger(num1, returnLarger(num2, num3)); //note the function calls
+    //__debug_break
+
+    return largest; 
+
+}
+
+//Note that it may be wasteful/expensive to pass string arguments this way
+//("passing by value") -more on this later
+bool isSameStringLength(string firstString, string secondString)
+{
+    if (firstString.length() == secondString.length()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
